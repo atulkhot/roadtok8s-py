d0954ad60147012ff67bf236b2c0a189e279b0fa
atulkhot <atul.khot@gmail.com>
2025-07-18 18:47:23 +0530

    Again Changed the greeting message

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def read_index():
     """
     Return a Python Dictionary that supports JSON serialization.
     """
-    return {"Hello": "on the Road"}
+    return {"Hello": "on the Road - most welcome!"}
 
 @app.get("/api/v1/hello-world/")
 def read_hello_world():
